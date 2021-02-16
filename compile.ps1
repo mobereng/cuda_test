9a549ce584e0add55066006097306205ab969cf5
Moisés Berenguer <ext_mobereng@mercadolibre.com.ve>
2021-02-15 21:59:09 -0400

    Replicating issue

--- compile.ps1
+++ compile.ps1
@@ -1,5 +1,4 @@
 New-Item -ItemType Directory -Force -Path build/
 Remove-Item build/* -Recurse -Confirm:$false -exclude .gitkeep
-#cmake -S . -B build/ -DCMAKE_GENERATOR_PLATFORM=x64 -DBOOST_ROOT=tools/boost/ -DThrust_DIR="../thrust/thrust/cmake/" -DEigen3_DIR="../eigen/share/eigen3/cmake/"
-cmake -S . -B build/ -DCMAKE_GENERATOR_PLATFORM=x64 -DBOOST_ROOT=tools/boost/ -DEigen3_DIR="../eigen/share/eigen3/cmake/"
+cmake -S . -B build/ -DCMAKE_GENERATOR_PLATFORM=x64 -DBOOST_ROOT="tools/boost/"
 MSBuild.exe build/CudaTest.sln -property:Configuration=Release -m
