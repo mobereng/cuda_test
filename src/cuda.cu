6ab8ac084b0bf7c494b4331a365dbb5958ba4ec4
Moisés Berenguer <ext_mobereng@mercadolibre.com.ve>
2021-02-15 22:43:29 -0400

    Minimal version

--- src/cuda.cu
+++ src/cuda.cu
@@ -6,6 +6,6 @@ __global__ void test_kernel(void) {
 namespace Wrapper {
 	void wrapper(void) {
 		test_kernel <<<1, 1>>> ();
-		printf("Hello World from GPU!");
+		printf("Hello World from GPU\n");
 	}
 }
